d2e0812158d4d4fc270c69e0f45ba53428416af7
akarmanov <arseniy.karmanov@mail.ru>
2022-02-18 10:20:58 +0900

    Initial commit

---
+++ src/main/java/com/stx/StxTestTaskRestApplication.java
@@ -0,0 +1,13 @@
+package com.stx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StxTestTaskRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StxTestTaskRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stx/StxTestTaskRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StxTestTaskRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
